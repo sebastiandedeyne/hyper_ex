9b95f02ae6de82e890830a2d0f3972697940dfd3
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-10-05 10:52:34 +0200

    Update mix

--- mix.exs
+++ mix.exs
@@ -1,28 +1,41 @@
 defmodule HyperEx.Mixfile do
   use Mix.Project
 
+  @version "0.3.0"
+
   def project do
     [
       app: :hyper_ex,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      description: "A YAML front matter parser for Elixir.",
+       package: package(),
+       deps: deps(),
+       docs: [extras: ["README.md"],
+              main: "readme",
+              source_ref: "v#{@version}",
+              source_url: "https://github.com/sebastiandedeyne/hyper_ex"]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
+  end
+
+  defp package do
+    [name: :hyper_ex,
+     files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+     maintainers: ["Sebastian De Deyne"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/sebastiandedeyne/hyper_ex"}]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
